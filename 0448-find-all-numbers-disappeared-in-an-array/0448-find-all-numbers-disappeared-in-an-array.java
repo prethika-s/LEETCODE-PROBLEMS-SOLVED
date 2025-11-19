6e51d86a05f339313567d157363683727f046e02
Prethika S <mailstosprethika@gmail.com>
2025-11-19 11:58:09 +0530

    Time: 27 ms (6.6%), Space: 71.1 MB (12.05%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -1,19 +1,37 @@
 class Solution {
-    public List<Integer> findDisappearedNumbers(int[] nums) {
-        List<Integer> res = new ArrayList<>();
-        if (nums == null || nums.length == 0) return res;
-        int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            int val = Math.abs(nums[i]);
-            if (val >= 1 && val <= n) {
-                int idx = val - 1;
-                if (nums[idx] > 0) nums[idx] = -nums[idx];
+    public List<Integer> findDisappearedNumbers(int[] arr) {
+        Arrays.sort(arr);
+        ArrayList<Integer> result = new ArrayList<>();
+        int counter = 0;
+        int prev = Integer.MIN_VALUE;
+        if(arr[0]!=1){
+            int s = arr[0];
+            for(int i=1; i<s; i++){
+                result.add(i);
             }
         }
-        for (int i = 0; i < n; i++) {
-            if (nums[i] > 0) res.add(i + 1);
+        for(int i=1; i<arr.length; i++){
+            if(arr[i]!=arr[i-1]){
+                int diff = arr[i]-arr[i-1];
+                if(diff>1){
+                    for(int j=arr[i-1]+1; j<arr[i];j++){
+                        result.add(j);
+                    }
+                }
+            }
         }
-
-        return res;
+        if(arr[arr.length-1]!=arr.length){
+            int start = arr[arr.length-1]+1;
+            for(int i=start; i<=arr.length; i++){
+                result.add(i);
+            }
+        }
+        if(arr.length==2 && result.size()==0){
+            if(arr[0]==arr[1]){
+                if(arr[0]!=1 && !result.contains(1)) result.add(1);
+                else if(!result.contains(arr[0]+1)) result.add(arr[0]+1);
+            }
+        }
+        return result;
     }
 }
