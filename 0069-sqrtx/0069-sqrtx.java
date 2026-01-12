08818be3795fdee3c9e71eae2612c55f1324e215
Prethika S <mailstosprethika@gmail.com>
2026-01-12 17:14:57 +0530

    Time: 1 ms (96.32%), Space: 42.4 MB (81.98%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,5 +1,19 @@
 class Solution {
-    public int mySqrt(int x) {
-        return (int) Math.sqrt(x);
+    public int mySqrt(int N) {
+        if(N==0 || N==1) return N;
+        int left = 1;
+        int right = N;
+        int sqr = left;
+        while(left<=right){
+            int mid = left+(right-left)/2;
+            if((long)mid*mid <= N){
+                sqr = mid;
+                left=mid+1;
+            }
+            else{
+                right=mid-1;
+            }
+        }
+        return sqr;
     }
 }
