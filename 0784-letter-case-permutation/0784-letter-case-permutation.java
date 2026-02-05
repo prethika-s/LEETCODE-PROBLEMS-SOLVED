f630e5638427c6225c7960443baaf85337366367
Prethika S <mailstosprethika@gmail.com>
2026-02-05 14:39:08 +0530

    Time: 1 ms (99.88%), Space: 47.4 MB (68.7%) - LeetHub

--- 0784-letter-case-permutation/0784-letter-case-permutation.java
+++ 0784-letter-case-permutation/0784-letter-case-permutation.java
@@ -3,25 +3,19 @@ class Solution {
         List<String> result = new ArrayList<>();
         int N = s.length();
         char[] string = s.toCharArray();
-
-        for (int i = 0; i < N; i++) {
-            string[i] = Character.toLowerCase(string[i]);
+        for(int i=0; i<N; i++){
+            string[i]= Character.toLowerCase(string[i]);
         }
-
         recursion(string, 0, result, N);
         return result;
     }
-
-    public void recursion(char[] string, int idx, List<String> result, int N) {
-        // add result at every recursion entry
+    public void recursion(char[] string, int idx, List<String> result, int N){
         result.add(new String(string));
-
-        for (int i = idx; i < N; i++) {
+        for(int i=idx; i<N; i++){
             if (Character.isDigit(string[i])) continue;
-
-            string[i] = Character.toUpperCase(string[i]);
-            recursion(string, i + 1, result, N);
-            string[i] = Character.toLowerCase(string[i]); // backtrack
+            string[i]=Character.toUpperCase(string[i]);
+            recursion(string, i+1, result, N);
+            string[i]=Character.toLowerCase(string[i]);
         }
     }
 }
