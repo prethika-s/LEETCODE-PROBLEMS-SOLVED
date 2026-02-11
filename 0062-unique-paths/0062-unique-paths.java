315c8339309137e29a66554d5f1d05cae27db38d
Prethika S <mailstosprethika@gmail.com>
2026-02-11 12:39:44 +0530

    Time: 22 ms (1.19%), Space: 43.3 MB (10.9%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,16 +1,27 @@
 class Solution {
-    public int dpfun(int m,int n, int[][]dp){
-        if(m==1 || n==1){
-            return 1;
-        }
-        if(dp[m][n]!=0){
-            return dp[m][n];
-        }
-        dp[m][n]=dpfun(m,n-1,dp)+dpfun(m-1,n,dp);
-        return dp[m][n];
-    }
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m+1][n+1];
-        return dpfun(m,n,dp);
+       int[][] dp = new int[m][n];
+       dp[m-1][n-1] = 1;
+       for(int row=m-1; row>=0; row--){
+        for(int col=n-1; col>=0; col--){
+            if(row==m-1 && col==n-1) continue;
+            if(row==m-1){
+                dp[row][col]=dp[row][col+1];
+            }
+            else if(col==n-1){
+                dp[row][col]=dp[row+1][col];
+            }
+            else{
+                dp[row][col]=dp[row+1][col]+dp[row][col+1];
+            }
+        }
+       } 
+       for(int row=0; row<m; row++){
+        for(int col=0; col<n; col++){
+            System.out.print(dp[row][col]+" ");
+        }
+        System.out.println();
+       }
+       return dp[0][0];
     }
 }
