02c4a4115653eec8810f4c6332f52c9efca45226
Prethika S <mailstosprethika@gmail.com>
2026-04-07 21:36:57 +0530

    Time: 122 ms (14.32%), Space: 44.3 MB (86.1%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -1,23 +1,21 @@
 class Solution {
+    private int calcCoins(int[] nums, int l, int r, int[][] dp){
+        if(l>r) return 0;
+        if(dp[l][r]!=-1) return dp[l][r];
+        int max = Integer.MIN_VALUE;
+        for(int k=l; k<=r; k++){
+            int cost = (l-1<0 ? 1 : nums[l-1])*nums[k]*(r+1<nums.length? nums[r+1] : 1 )+calcCoins(nums, l, k-1, dp)+calcCoins(nums, k+1, r, dp);
+            max = Math.max(cost, max);
+        }
+        dp[l][r]=max;
+        return dp[l][r];
+    }
     public int maxCoins(int[] nums) {
-        int n=nums.length;
-        int[] arr=new int[n+2];
-        arr[0]=1;
-        arr[n+1]=1;
-        for(int i=1;i<=n;i++){
-            arr[i]=nums[i-1];
+        int N = nums.length;
+        int dp[][] = new int[N][N];
+        for(int[] row: dp){
+            Arrays.fill(row, -1);
         }
-        int[][] dp=new int[n+2][n+2];
-        for(int len=2;len<n+2;len++) {
-            for(int left=0;left+len<n+2;left++) {
-                int right=left+len;
-                for(int k=left+1;k<right;k++){
-                    int coins=arr[left]*arr[k]*arr[right];
-                    coins+=dp[left][k]+dp[k][right];
-                    dp[left][right]=Math.max(dp[left][right], coins);
-                }
-            }
-        }
-        return dp[0][n + 1];
+        return calcCoins(nums, 0, N-1, dp);
     }
 }
